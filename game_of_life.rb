2de512983a542b2c09a3e9373163a91327bbd866
willcab <willianeduardocg13@gmail.com>
2022-08-15 18:34:24 -0500

    refact class Cell
    Co-authored-by: Brandon2002 <btapia@ucol.mx>
    Co-authored-by: CalixtoLopez <calixto.lopez.santos@gmail.com>
    Co-authored-by: AlfredoPazZamora <apaz0@ucol.mx>

--- game_of_life.rb
+++ game_of_life.rb
@@ -2,7 +2,7 @@ class Table
     def initialize(rows, columns) 
         @rows = rows
         @columns = columns
-        @table = Array.new(rows){Array.new(columns){Cell.new().random_state}}
+        @table = Array.new(rows){Array.new(columns){Cell.new()}} #. *
     end
 
     def print_table
@@ -16,16 +16,18 @@ class Table
 end
 
 class Cell
-    def initialize()
-        @state = '.'
-        
+    def initialize(state=nil)
+    
+        @state = state.nil? ? random_state : state
     end
     def random_state
         type_of_cell =['*','.']
-        @state = type_of_cell[rand(2)]
+        type_of_cell[rand(2)]
 
     end
-    
+    def to_s
+        @state
+    end
 end
 
 Table.new(3, 3).print_table
