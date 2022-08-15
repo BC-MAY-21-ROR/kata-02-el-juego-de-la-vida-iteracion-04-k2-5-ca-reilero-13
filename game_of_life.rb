fa7daebe309f3a9ebffea6872aaf68392a4f70a2
Calixto <calixto.lopez.santos@gmail.com>
2022-08-16 01:06:36 +0200

    Create cell class
    co-authorized-by: AlfredoPazZamora <apaz0@ucol.mx>
    Co-authored-by: Brandon2002 <btapia@ucol.mx>
    Co-authored-by: willcab <willianeduardocg13@gmail.com>
    git commit -m

--- game_of_life.rb
+++ game_of_life.rb
@@ -2,7 +2,7 @@ class Table
     def initialize(rows, columns) 
         @rows = rows
         @columns = columns
-        @table = Array.new(rows){Array.new(columns){"."}}
+        @table = Array.new(rows){Array.new(columns){Cell.new().random_state}}
     end
 
     def print_table
@@ -13,6 +13,19 @@ class Table
             puts 
         end
     end
+end
+
+class Cell
+    def initialize()
+        @state = '.'
+        
+    end
+    def random_state
+        type_of_cell =['*','.']
+        @state = type_of_cell[rand(2)]
+
+    end
+    
 end
 
 Table.new(3, 3).print_table
